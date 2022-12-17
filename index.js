606966fbf4670912930a841ec7da7057139ffe8d
Matheus Soares <matheussilvasoares43@gmail.com>
2022-12-17 13:04:06 -0300

    gerando imagem

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+//Recebendo dados da requisição
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, () => console.log(`Servidor aberto na porta ${port}`))
